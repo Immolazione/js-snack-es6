9ff999daebf810d5a727ea745ea5ee16a8ba4f85
Immolazione <federico.deered@gmail.com>
2022-01-20 16:39:08 +0100

    add optional solution

--- js/script1.js
+++ js/script1.js
@@ -12,25 +12,38 @@ const bicycles = [
     {nome: 'bici1', peso: 50}, 
     {nome: 'bici2', peso: 30}, 
     {nome: 'bici3', peso: 50}, 
-    {nome: 'bici4', peso: 10}, 
+    {nome: 'bici4', peso: 5}, 
     {nome: 'bici5', peso: 80}, 
     {nome: 'bici6', peso: 40}
 ];
 console.table(bicycles);
 
-// IMPOSTO IL VALORE INIZIALE COME VALORE MINIMO
-let actualWeight = bicycles[0].peso;
+// // IMPOSTO IL VALORE INIZIALE COME VALORE MINIMO
+// let actualWeight = bicycles[0].peso;
 
-// CICLO L'ARRAY
-for (i = 0; i < bicycles.length; i++){
+// // CICLO L'ARRAY
+// for (i = 0; i < bicycles.length; i++){
 
-    let peso = bicycles[i].peso;
+//     let peso = bicycles[i].peso;
 
-    // METTO A CONFRONTO
-    if (peso < actualWeight) {
-        actualWeight = peso;
+//     // METTO A CONFRONTO
+//     if (peso < actualWeight) {
+//         actualWeight = peso;
+//     }
+
+// };
+
+// console.log(actualWeight);
+
+// SOLUZIONE ALTERNATIVA TENENDO CONTO DELL'INDICE
+let minorPeso = 0;
+
+for (i = 0; i < bicycles.length - 1; i++) {
+
+    if (bicycles[i].peso < bicycles[minorPeso].peso) {
+        minorPeso = i;
     }
 
 };
 
-console.log(actualWeight);
+console.log(bicycles[minorPeso].peso);
