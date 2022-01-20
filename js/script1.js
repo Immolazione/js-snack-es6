15ed2bad45ba677af18e95c0d5d54c3897b87c18
Immolazione <federico.deered@gmail.com>
2022-01-20 15:00:16 +0100

    add array

--- js/script1.js
+++ js/script1.js
@@ -4,6 +4,16 @@ SNACK1
 
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare ain console la bici con peso minore utilizzando destructuring e template literal.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal.
 */
+
+// ARRAY DI BICI
+const bicycles = [
+    {nome: 'bici1', peso: 20}, 
+    {nome: 'bici2', peso: 30}, 
+    {nome: 'bici3', peso: 50}, 
+    {nome: 'bici4', peso: 10}, 
+    {nome: 'bici5', peso: 80}, 
+    {nome: 'bici6', peso: 40}
+];
 
