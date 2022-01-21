6cf9a2f7914b7ecbff9b012e19f575c9ccaeebeb
Immolazione <federico.deered@gmail.com>
2022-01-21 17:39:26 +0100

    attempt to solve snack3

--- js/script3.js
+++ js/script3.js
@@ -7,3 +7,17 @@ La funzione ritornerà un nuovo array con i valori che hanno la posizione compre
 Usiamo i nuovi metodi degli array visti oggi in classe.
 */
 
+const array = [3, 45, 3, 6, 1, 76, 34, 9, 10, 87, 234, 76];
+console.table(array);
+
+function selected(array, a, b){
+   array.forEach((item, a, b) => {
+    const newArray = array.filter((item, a, b) => {
+        if (item < a || item > b) {
+            return false
+        };
+        return newArray;
+    });
+    console.log(newArray);
+   });
+};
