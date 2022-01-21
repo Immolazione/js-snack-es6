4a74fb2381c0071404e105415dc738e1a6971ca6
Immolazione <federico.deered@gmail.com>
2022-01-21 15:59:21 +0100

    add students array

--- js/script4.js
+++ js/script4.js
@@ -8,3 +8,19 @@ ES (marco de iulio => Marco de iulio);
 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 */
+
+// ARRAY DI STUDENTI
+const studenti = [
+    {nome: 'Giulia', id: '100', voto: '50'},
+    {nome: 'Enrico', id: '200', voto: '80'},
+    {nome: 'Federico', id: '150', voto: '60'},
+    {nome: 'Marco', id: '70', voto: '75'},
+    {nome: 'Matteo', id: '20', voto: '100'},
+    {nome: 'Marco', id: '105', voto: '40'},
+    {nome: 'Ignazio', id: '80', voto: '95'},
+    {nome: 'Giulio', id: '120', voto: '25'},
+    {nome: 'Bernardo', id: '10', voto: '55'}
+];
+
+console.table(studenti);
+
